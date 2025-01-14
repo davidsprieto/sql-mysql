bab2b72de4962fe4baab37bab662cef177d245e8
David Prieto <david.s.prieto1@gmail.com>
2025-01-14 12:57:55 -0600

    completed section 3 of MySQL refresher

--- Section3/CreatingAndTables.sql
+++ Section3/CreatingAndTables.sql
@@ -12,3 +12,23 @@ USE Test_db;
 
 SELECT database();
 
+CREATE TABLE users
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (username),
+    UNIQUE (email)
+);
+
+SHOW TABLES;
+
+SHOW COLUMNS FROM users;
+
+DESC users;
+
+DROP TABLE users;
+
+SHOW TABLES;
